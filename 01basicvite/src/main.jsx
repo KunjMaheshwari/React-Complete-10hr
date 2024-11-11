08791bd03372cc585d29ad8ffed28da9eaef5d14
Kunj Maheshwari <kunj.maheshwari2021@vitbhopal.ac.in>
2024-11-11 23:04:09 +0530

    Created areactapp function.

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,6 +1,35 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <App />
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href : "https://google.com",
+        target : '_blank'
+    },
+    children : 'Click me to visit google'
+}
+
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom React App</h1>
+        </div>
+    )
+}
+
+const AnotherElement = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const areactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click to visit google'
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App/>
 )
